d07f46a43a3865b9b483fd5c07d0ecb1ac310eda
davidnghi <49269680+davidnghi@users.noreply.github.com>
2021-10-07 23:14:37 +0700

    update web 07-10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-use strict;
+$(function(){
+	var position = $('#id-skill').offset().top;
+	console.log(position);
+	$('.btn-down').click(function(){
+		$('body,html').animate({scrollTop:position},600)
+	})
+})
