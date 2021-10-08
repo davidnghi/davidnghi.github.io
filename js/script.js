82957665d1821484706c5abb699766cdcafb0d04
davidnghi <49269680+davidnghi@users.noreply.github.com>
2021-10-08 10:58:11 +0700

    back to top

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 $(function(){
 	var position = $('#id-skill').offset().top;
-	console.log(position);
+	//console.log(position);
 	$('.btn-down').click(function(){
 		$('body,html').animate({scrollTop:position},600)
 	})
+
+
+		//Get the button
+	let mybutton = document.getElementById("btn-back-to-top");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+	  scrollFunction();
+	};
+
+	function scrollFunction() {
+	  if (
+	    document.body.scrollTop > 20 ||
+	    document.documentElement.scrollTop > 20
+	  ) {
+	    mybutton.style.display = "block";
+	  } else {
+	    mybutton.style.display = "none";
+	  }
+	}
+	// When the user clicks on the button, scroll to the top of the document
+	mybutton.addEventListener("click", backToTop);
+
+	function backToTop() {
+	  document.body.scrollTop = 0;
+	  document.documentElement.scrollTop = 0;
+	}
 })
